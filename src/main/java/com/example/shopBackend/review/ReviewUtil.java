cdb619ab2d9d42360ed5725321a55efe1468e549
Iiro Partanen <iispar@utu.fi>
2023-07-24 18:23:14 +0300

    makes call to api and displays it in string json

--- src/main/java/com/example/shopBackend/review/ReviewUtil.java
+++ src/main/java/com/example/shopBackend/review/ReviewUtil.java
@@ -1,9 +1,26 @@
 package com.example.shopBackend.review;
 
+import net.minidev.json.JSONObject;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
 import java.util.List;
 
 public class ReviewUtil {
     public static void rateReviews(List<String> reviews) {
-        System.out.println(reviews);
+        JSONObject jsonReviews = new JSONObject();
+        jsonReviews.put("reviews", reviews);
+
+
+        WebClient client = WebClient.create("http://127.0.0.1:8000");
+
+        String ratedReviews = client.post()
+                .uri("/rate")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(jsonReviews)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+        System.out.println(ratedReviews);
     }
 }
